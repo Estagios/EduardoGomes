febc5a2c33140e0b6272aae01d7ae7367cbeef75
Eduardo Gomes <eamgedu@gmail.com>
2020-06-04 11:16:45 +0100

    AdventOfCode2015

---
+++ Dia 4/Dia 4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace MD5Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "ckczppom";
+            int answer = 0;
+            int i = 0;
+            using (MD5 md5Hash = MD5.Create())
+            {
+                var hash = string.Join("", MD5.Create().ComputeHash(Encoding.ASCII.GetBytes(text)).Select(s => s.ToString("x2")));
+
+                while (hash.Substring(0, +6) != "000000")
+                {
+                    if (i == 0)
+                    {   
+                        if (hash.Substring(0, +5) == "00000")
+                        {
+                            i++;
+                            Console.WriteLine("Esta é a " + i + "ª chave para o hash com 5 zeros: " + hash + " : " + answer);
+                            Console.WriteLine("Pressione qualquer tecla avançar.");
+                            Console.WriteLine();
+                            Console.ReadKey();
+                        }
+                    }
+                    answer++;
+                    hash = string.Join("", MD5.Create().ComputeHash(Encoding.ASCII.GetBytes(text+answer)).Select(s => s.ToString("x2")));
+                }
+                Console.WriteLine("Esta é chave para o hash com 6 zeros: " + hash + " : " + answer);
+                Console.WriteLine("Pressione qualquer tecla terminar o programa.");
+                Console.ReadKey();
+            }
+        }      
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Dia_5
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var text = File.ReadAllLines(@"Dia 5.txt");
+
+            MM(text);
+            return;
+
+        }
+
+        private static void MM(string[] text)
+        {
+            int count = 0;
+
+            foreach (var line in text)
+            {
+                Console.WriteLine(line);
+                bool firstCond = CalcFirstCond(line);
+                var thirdCond = !line.Contains("ab") && !line.Contains("cd") && !line.Contains("pq") && !line.Contains("xy");
+                var secCond = false;
+                for (int i = 0; i < line.Length - 1; i++)
+                {
+                    secCond = line[i] == line[i + 1];
+                    if (secCond)
+                        break;
+                }
+
+                if (firstCond && secCond && thirdCond)
+                    count++;
+
+                Console.WriteLine(firstCond);
+                Console.WriteLine(secCond);
+                Console.WriteLine(thirdCond);
+            }
+
+            Console.WriteLine(count);
+        }
+
+        private static bool CalcFirstCond(string line)
+        {
+            return line.Count(p => p == 'a' || p == 'e' || p == 'i' || p == 'o' || p == 'u') >= 3;
+        }
+    }
+}
