8829f4713826d74bbe964e570f1ca590bb2a8b93
Eduardo Gomes <eamgedu@gmail.com>
2020-07-08 16:13:52 +0100

    last commit

--- Dia 6/Program.cs
+++ Dia 6/Program.cs
@@ -4,9 +4,8 @@ using System.Linq;
 
 namespace Dia_6
 {
-     class Program
+    internal class Program
     {
-
         public struct Begin
         {
             public bool x, y;
@@ -19,8 +18,8 @@ namespace Dia_6
 
         public static void Main(string[] args)
         {
-            var text = File.ReadAllLines(@"Dia 6.txt");
-
+            var text = File.ReadAllLines("Dia 6 - Test1.txt");
+            //var text = File.ReadAllLines(@"Dia 6.txt");
             MM(text);
             return;
         }
@@ -35,25 +34,22 @@ namespace Dia_6
                 string[] words = line.Split(' ');
                 if (words.Contains("toggle"))
                 {
-                    var aux = words[1].Split(',');
-                    var eaux = words[3].Split(',');
-                    var exaux = Int32.Parse(eaux[0]);
-                    var eyaux = Int32.Parse(eaux[1]);
-                    var xaux = Int32.Parse(aux[0]);
-                    var yaux = Int32.Parse(aux[1]);
+                    int exaux, eyaux, xaux, yaux;
+                    NewMethod(words, out exaux, out eyaux, out xaux, out yaux);
                     for (int i = xaux; i < exaux; i++)
                     {
                         for (int j = yaux; j < eyaux; j++)
                         {
                             if (lights[i, j] == 1)
                                 lights[i, j] = 0;
-                            if (lights[i, j] == 0)
+                            else if (lights[i, j] == 0)
                                 lights[i, j] = 1;
                         }
                     }
                 }
+
                 if (words.Contains("turn off"))
-                    {
+                {
                     var aux = words[2].Split(',');
                     string[] eaux = words[4].Split(',');
                     var exaux = Int32.Parse(eaux[0]);
@@ -90,6 +86,16 @@ namespace Dia_6
             }
             Console.WriteLine(count);
             Console.ReadKey();
+        }
+
+        private static void NewMethod(string[] words, out int exaux, out int eyaux, out int xaux, out int yaux)
+        {
+            var aux = words[1].Split(',');
+            var eaux = words[3].Split(',');
+            exaux = Int32.Parse(eaux[0]);
+            eyaux = Int32.Parse(eaux[1]);
+            xaux = Int32.Parse(aux[0]);
+            yaux = Int32.Parse(aux[1]);
         }
     }
 }
